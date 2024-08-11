69fcbf7232112f296c97570fb8cfaec97731d4a8
Stefan Probst <stefanprobst@posteo.de>
2024-08-11 09:54:55 +0200

    add repro

---
+++ app.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { Slider } from "@ark-ui/vue";
+</script>
+
+<template>
+  <main>
+    <!-- @see https://ark-ui.com/vue/docs/components/slider#range-slider -->
+    <Slider.Root name="time">
+      <Slider.Label>Label</Slider.Label>
+      <Slider.ValueText />
+      <Slider.Control>
+        <Slider.Track>
+          <Slider.Range />
+        </Slider.Track>
+        <Slider.Thumb :index="0">
+          <Slider.HiddenInput />
+        </Slider.Thumb>
+        <Slider.Thumb :index="1">
+          <Slider.HiddenInput />
+        </Slider.Thumb>
+      </Slider.Control>
+    </Slider.Root>
+  </main>
+</template>
